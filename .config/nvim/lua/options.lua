b18f7832f2b3dfe6f911c816aefe592a2baa09af
succSeeded <anakin03@mail.ru>
2025-07-06 13:15:54 +0300

    Fix neovim tabsize

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,15 +2,16 @@ require "nvchad.options"
 
 -- add yours here!
 
+local o = vim.o
 local g = vim.g
 
 g.tex_flavor = "latex"
 g.vimtex_view_method = "zathura"
 g.vimtex_quickfix_mode = 0
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+o.tabstop = 2 -- A TAB character looks like 2 spaces
+o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
