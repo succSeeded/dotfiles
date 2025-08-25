0fb2927d91d6806b2ed1b5625de4000bb8e35b02
succSeeded <anakin03@mail.ru>
2025-08-25 18:53:34 +0300

    Updated waybar and power menu

--- .config/scripts/rofi/powermenu-hypr.sh
+++ .config/scripts/rofi/powermenu-hypr.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
-chosen=$(printf "󰐥  Power off\n󰑐  Reboot\n  Hibernate\n󰗽  Log Out\n󰍁  Lock Screen\n" | rofi -dmenu -i -theme-str '@import "power.rasi"')
+chosen=$(printf "󰐥  Power off\n󰑐  Reboot\n  Hibernate\n󰗽  Log Out\n󰍁  Lock Screen\n󰒲  Suspend\n" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 case "$chosen" in
 	"󰐥  Power off") shutdown now ;;
 	"󰑐  Reboot") reboot ;;
 	"  Hibernate") systemctl hibernate ;;
 	"󰗽  Log Out") hyprctl dispatch exit ;;
-	"󰍁  Lock Screen") hyprlock ;; # 
+	"󰍁  Lock Screen") hyprlock ;; 
+  "󰒲  Suspend") systemctl suspend ;; #
 	*) exit 1 ;;
 esac
