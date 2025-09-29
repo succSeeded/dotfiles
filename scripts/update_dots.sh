4b198fdfc9281e5cb1ca71fd0ccc7820deb0a842
succSeeded <m.kirdin@yandex.ru>
2025-09-29 18:55:01 +0000

    fix: change waybar copying directory

--- scripts/update_dots.sh
+++ scripts/update_dots.sh
@@ -4,7 +4,7 @@ cp -r ${HOME}/.config/hypr ${HOME}/cloned-repos/dotfiles
 cp -r ${HOME}/.config/kitty ${HOME}/cloned-repos/dotfiles
 cp -r ${HOME}/.config/mako ${HOME}/cloned-repos/dotfiles
 cp -r ${HOME}/.config/nvim ${HOME}/cloned-repos/dotfiles
-cp -r ${HOME}/.config/waybar ${HOME}/cloned-repos/dotfiless
+cp -r ${HOME}/.config/waybar ${HOME}/cloned-repos/dotfiles
 cp -r ${HOME}/.config/wofi ${HOME}/cloned-repos/dotfiles
 cp -r ${HOME}/.config/zathura ${HOME}/cloned-repos/dotfiles
 cp ${HOME}/.bashrc ${HOME}/cloned-repos/dotfiles
