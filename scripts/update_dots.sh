49122fd4358b0afe2e2c59397630039ee7cc3a7d
succSeeded <mdkirdin@yandex.ru>
2025-10-26 21:19:46 +0300

    feat: add GTK theme to dotfiles

--- scripts/update_dots.sh
+++ scripts/update_dots.sh
@@ -7,6 +7,10 @@ cp -r ${HOME}/.config/nvim ${HOME}/cloned-repos/dotfiles
 cp -r ${HOME}/.config/waybar ${HOME}/cloned-repos/dotfiles
 cp -r ${HOME}/.config/wofi ${HOME}/cloned-repos/dotfiles
 cp -r ${HOME}/.config/zathura ${HOME}/cloned-repos/dotfiles
+cp -r ${HOME}/.config/gnome-shell ${HOME}/cloned-repos/dotfiles
+cp -r ${HOME}/.config/gtk\-4\.0 ${HOME}/cloned-repos/dotfiles
+cp -r ${HOME}/.config/gtk\-3\.0 ${HOME}/cloned-repos/dotfiles
+cp -r ${HOME}/.config/index.theme ${HOME}/cloned-repos/dotfiles
 cp -r $HOME/.config/scripts $HOME/cloned-repos/dotfiles
 cp ${HOME}/.bashrc ${HOME}/cloned-repos/dotfiles
 cp ${HOME}/.zshrc ${HOME}/cloned-repos/dotfiles
