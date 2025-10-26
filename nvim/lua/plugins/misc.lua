e5483c0f886b6efe263d4da1f3a91300df404f77
succSeeded <mdkirdin@yandex.ru>
2025-10-26 21:18:31 +0300

    feat: add plugin for LaTeX

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -16,6 +16,15 @@ return {
 	},
 
 	{
+		"lervag/vimtex",
+		lazy = false, -- we don't want to lazy load VimTeX
+		init = function()
+			-- VimTeX configuration goes here, e.g.
+			vim.g.vimtex_view_method = "zathura"
+		end
+	},
+
+	{
 		'chomosuke/typst-preview.nvim',
 		opts = {}
 	},
